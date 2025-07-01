1623ae90fcd1d2ddc5fc0848ee5f37e26e24ee55
dadnaa <df.madhoui@esi-sba.dz>
2025-07-01 22:58:29 +0100

    fix Link

--- safespace/src/components/navBAr.jsx
+++ safespace/src/components/navBAr.jsx
@@ -24,7 +24,7 @@ const NavBar = () => {
         ]
       : role === "Admin"
       ? [
-          { page: "Home", link: "/home" },
+          { page: "Home", link: "/" },
           { page: "Dashboard", link: "/dash" },
         ]
       : [
